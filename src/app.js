6ef2b0e40aa7410d2230532220cc2abf19a81315
212735196 <nikki.adyani@ge.com>
2020-09-12 10:05:46 -0400

    chat works

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
-app.use(express.static('src/public'));
+app.use(express.static(__dirname + '/public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
+});
+let messages = [];
+app.post("/send-message", (req, res) => {
+    // console.log(req.body); 
+    messages.push(req.body);
+    // res.send(messages);
+    res.send(req.body);
+  
 });
 
 const port = process.env.PORT || 3000;
--- src/public/message.js
+++ src/public/message.js
@@ -1,4 +1,20 @@
-const message = "";
-function getMessage(){
-    message = document.getElementById("text-input").value;
-}
+const formObj = document.getElementById("chat-form");
+formObj.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  let text = document.getElementById("text-input").value;
+  document.getElementById("text-input").value = '';
+  const response = await fetch("/send-message", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
+
+  const msgJson = await response.json();
+  document.getElementById("messages").value += msgJson.text;
+  document.getElementById("messages").value += "\r\n";
+//   msgJson.forEach((msg) => {
+//     document.getElementById("messages").value += msg.text;
+//   });
+});
